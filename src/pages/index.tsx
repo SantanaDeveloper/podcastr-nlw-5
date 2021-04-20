5eb7ad7490a75b7f11082593288f23972732875d
João Barbosa <joao.barbosa@managerbs.com>
2021-04-20 14:33:42 -0300

    Create fake api.

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,44 @@
-import Header from "../components/Header";
+//SPA
+//SSR
+//SSG
 
-export default function Home() {
+import { useEffect } from "react";
+
+const index = ({ episodes }) => {
+  // useEffect(() => {
+  //   fetch("http://localhost:3333/episodes")
+  //     .then((response) => response.json())
+  //     .then((data) => console.log(data));
+  // }, []);
   return (
-    <Header />
-  )
+    <>
+      <h1>Index</h1>
+      <p>{JSON.stringify(episodes)}</p>
+    </>
+  );
+};
+
+export default index;
+
+// export async function getServerSideProps() {
+//   const response = await fetch("http://localhost:3333/episodes");
+//   const data = await response.json();
+
+//   return {
+//     props: {
+//       episodes: data,
+//     },
+//   };
+// }
+
+export async function getStaticProps() {
+  const response = await fetch("http://localhost:3333/episodes");
+  const data = await response.json();
+
+  return {
+    props: {
+      episodes: data,
+    },
+    revalidate: 60 * 60 * 8,
+  };
 }
